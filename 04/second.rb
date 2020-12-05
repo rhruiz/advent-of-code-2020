01e145646a6444ceeda91dfada8c460d9e6c0271
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2020-12-05 00:24:48 -0300

    shuffle things around

--- 04/second.rb
+++ 04/second.rb
@@ -1,6 +1,6 @@
 require 'set'
 
-REQUIRED = Set.new(%w[byr iyr eyr hgt hcl ecl pid cid])
+REQUIRED = Set.new(%w[byr iyr eyr hgt hcl ecl pid])
 
 passports = File
   .open("input.txt")
@@ -18,7 +18,7 @@ def valid?(passport)
   keys = passport.dup
   keys.delete("cid")
   keys = keys.keys
-  Set.new(keys) == REQUIRED.delete("cid")
+  Set.new(keys) == REQUIRED && REQUIRED.all? { |field| valid_field?(passport, field) }
 end
 
 def valid_field?(passport, field)
@@ -26,6 +26,9 @@ def valid_field?(passport, field)
     byr: ->(value) { value.length == 4 && (1920..2002).member?(value.to_i) },
     iyr: ->(value) { value.length == 4 && (2010..2020).member?(value.to_i) },
     eyr: ->(value) { value.length == 4 && (2020..2030).member?(value.to_i) },
+    hcl: ->(value) { value =~ /\A#[0-9a-f]{6}\z/ },
+    ecl: ->(value) { %w[amb blu brn gry grn hzl oth].member?(value) },
+    pid: ->(value) { value =~ /\A[0-9]{9}\z/ },
     hgt: ->(value) { case value
                        when /\A(\d+)in\z/
                          (59..76).member?($1.to_i)
@@ -33,14 +36,10 @@ def valid_field?(passport, field)
                          (150..193).member?($1.to_i)
                        else
                          false
-                     end},
-    hcl: ->(value) { value =~ /\A#[0-9a-f]{6}\z/ },
-    ecl: ->(value) { %w[amb blu brn gry grn hzl oth].member?(value) },
-    pid: ->(value) { value =~ /\A[0-9]{9}\z/ },
-    cid: ->(_) { true }
+                     end}
   }
 
-  (!!validations[field.to_sym].call(passport[field]))
+  !!validations[field.to_sym].call(passport[field])
 end
 
-puts passports.select { |p| valid?(p) && REQUIRED.all? { |field| valid_field?(p, field) } }.count
+puts passports.select { |p| valid?(p) }.count
