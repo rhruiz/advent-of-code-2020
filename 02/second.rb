d9ef1f32fe62661b8cd87cb8f27ea44e1d4439a1
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2020-12-04 23:10:56 -0300

    Day 2 second star

---
+++ 02/second.rb
@@ -0,0 +1,7 @@
+puts(File.open("input.txt").lines.select do |line|
+  (first, second, letter, password) = line.scan(/^(\d+)\-(\d+) (\w): (\w+)$/).first
+  first = first.to_i - 1
+  second = second.to_i - 1
+
+  (password.chars[first] == letter) ^ (password.chars[second] == letter)
+end.size)
