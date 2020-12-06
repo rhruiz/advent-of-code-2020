b702a8f92e3026088441f83c9876d7f685f44334
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2020-12-06 12:13:24 -0300

    Add tests

--- 06/first.rb
+++ 06/first.rb
@@ -1,12 +1,22 @@
 require 'set'
+require 'minitest/unit'
 
-puts 6763 == File
-  .read("input.txt")
-  .split(/\n\n/)
-  .map { |group|
-    Set.new(group.lines.map(&:strip).flat_map(&:chars))
-  }
-    .tap  { |e| puts e.inspect }
+def count_yes_anwers(file)
+  File
+    .read(file)
+    .split(/\n\n/)
+    .map { |group| Set.new(group.lines.map(&:strip).flat_map(&:chars)) }
     .map(&:length)
-    .tap  { |e| puts e.inspect }
     .reduce(&:+)
+end
+
+
+class YesCounterTest < Minitest::Test
+  def test_with_sample_input
+    assert_equal 11, count_yes_anwers("input.sample.txt")
+  end
+
+  def test_with_input
+    assert_equal 6763, count_yes_anwers("input.txt")
+  end
+end
--- 06/second.rb
+++ 06/second.rb
@@ -1,17 +1,28 @@
 require 'set'
+require 'minitest/unit'
 
-puts 3512 == File
-  .read("input.txt")
-  .split(/\n\n/)
-  .map { |group|
-    answers = group.lines.map(&:strip).map(&:chars)
+def count_all_yes(file)
+  File
+    .read(file)
+    .split(/\n\n/)
+    .map { |group|
+      answers = group.lines.map(&:strip).map(&:chars)
 
-    [answers.length, answers.each_with_object(Hash.new { |_key| 0 }) do |answer, acc|
-      answer.each { |letter| acc[letter] += 1 }
-    end]
-  }
+      [answers.length, answers.each_with_object(Hash.new { |_key| 0 }) do |answer, acc|
+        answer.each { |letter| acc[letter] += 1 }
+      end]
+    }
     .map { |(size, answers)| answers.keep_if { |_letter, count| count == size } }
-    .tap  { |e| puts e.inspect }
     .map(&:length)
-    .tap  { |e| puts e.inspect }
     .reduce(&:+)
+end
+
+class CountAllYes < Minitest::Test
+  def test_with_sample_input
+    assert_equal 6, count_all_yes("input.sample.txt")
+  end
+
+  def test_with_input
+    assert_equal 3512, count_all_yes("input.txt")
+  end
+end
