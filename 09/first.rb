0f53bdee1eabc5fe8dcc26469d5748cd0e0e7816
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2020-12-09 11:02:52 -0300

    Day 09 first star

---
+++ 09/first.rb
@@ -0,0 +1,28 @@
+def parse(file)
+  File.open(file, 'r').each_line.map { |line| line.strip.to_i }.to_a
+end
+
+def find_first_match(numbers, start_at, range)
+  target = numbers[start_at+range]
+
+  puts "finding #{target}"
+
+  numbers
+    .drop(start_at)
+    .take(range)
+    .combination(2)
+    .find { |(n, m)| n != m && n + m == target }
+end
+
+def find_broken(numbers, range)
+  index = (0..(numbers.length-range)).find do |n|
+    find_first_match(numbers, n, range) == nil
+  end
+
+  numbers[index+range]
+end
+
+puts find_broken(parse('input.sample.txt'), 5)
+
+puts find_broken(parse('input.txt'), 25) # not 556543474
+
