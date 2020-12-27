bdfd7fa2a2a75abc3bd593adcd9725047dd5dfbf
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2020-12-27 17:21:34 -0300

    Refactor 20/2

--- 20/second.ex
+++ 20/second.ex
@@ -10,54 +10,8 @@ defmodule Camera do
   defmodule Picture do
     defstruct [:lines]
 
-    def variations(picture) do
-      picture
-      |> Stream.iterate(&rotate/1)
-      |> Stream.take(4)
-      |> Stream.concat(Stream.iterate(flip(picture), &rotate/1) |> Stream.take(4))
-    end
-
     def side(%__MODULE__{lines: lines}) do
       length(lines)
-    end
-
-    def render(%__MODULE__{lines: lines} = picture) do
-      lines
-      |> Enum.map(fn int ->
-        int
-        |> Integer.to_string(2)
-        |> String.pad_leading(side(picture), "0")
-      end)
-      |> Enum.join("\n")
-      |> String.replace("0", ".")
-      |> String.replace("1", "#")
-      |> IO.puts()
-    end
-
-    def column(%__MODULE__{lines: lines}, index) do
-      column(lines, index)
-    end
-
-    def column(lines, index) do
-      max = length(lines) - 1
-
-      Enum.reduce(lines, 0, fn line, acc ->
-        acc <<< 1 ||| (line >>> (max - index) &&& 1)
-      end)
-    end
-
-    def rotate(picture) do
-      max = length(picture.lines) - 1
-
-      new(
-        Enum.map(max..0, fn index ->
-          column(picture, index)
-        end)
-      )
-    end
-
-    def flip(picture) do
-      new(Enum.reverse(picture.lines))
     end
 
     def new(lines) do
@@ -67,54 +21,15 @@ defmodule Camera do
 
   defmodule Tile do
     @derive {Inspect, only: [:id, :borders]}
-    @max 9
 
-    defstruct [:id, :lines, :borders, :borders_flipped]
-
-    def vflip(tile) do
-      new(tile.id, Enum.reverse(tile.lines))
-    end
-
-    def borders(tile), do: tile.borders
-
-    def rotate(tile) do
-      max = length(tile.lines) - 1
-
-      new(
-        tile.id,
-        Enum.map(max..0, fn index ->
-          column(tile, index)
-        end)
-      )
-    end
-
-    def column(%__MODULE__{lines: lines}, index) do
-      column(lines, index)
-    end
-
-    def column(lines, index) do
-      max = length(lines) - 1
-
-      Enum.reduce(lines, 0, fn line, acc ->
-        acc <<< 1 ||| (line >>> (max - index) &&& 1)
-      end)
-    end
+    defstruct [:id, :lines, :borders]
 
     def new(id, lines) do
-      left = column(lines, 0)
-      right = column(lines, length(lines) - 1)
+      left = Camera.column(lines, 0)
+      right = Camera.column(lines, length(lines) - 1)
       borders = [hd(lines), right, lines |> List.last(), left]
-      borders_flipped = Enum.map(borders, &flip/1)
 
-      %__MODULE__{id: id, lines: lines, borders: borders, borders_flipped: borders_flipped}
-    end
-
-    def flip(int), do: flip(int, 0, 0)
-
-    def flip(_int, acc, count) when count > @max, do: acc
-
-    def flip(int, acc, count) do
-      flip(int, acc <<< 1 ||| (int >>> count &&& 1), count + 1)
+      %__MODULE__{id: id, lines: lines, borders: borders}
     end
   end
 
@@ -132,7 +47,7 @@ defmodule Camera do
           line
           |> String.split("", trim: true)
           |> Enum.reduce(0, fn chr, acc ->
-            (acc <<< 1) ^^^ if chr == "#", do: 1, else: 0
+            (acc <<< 1) ^^^ if(chr == "#", do: 1, else: 0)
           end)
         end)
 
@@ -140,40 +55,75 @@ defmodule Camera do
     end)
   end
 
-  def variations(tile) do
-    tile
-    |> Stream.iterate(&Tile.rotate/1)
-    |> Stream.take(4)
-    |> Stream.concat(Stream.iterate(Tile.vflip(tile), &Tile.rotate/1) |> Stream.take(4))
+  def flip(%Tile{} = tile) do
+    Tile.new(tile.id, Enum.reverse(tile.lines))
   end
 
-  def find_candidate(tiles, used, target, target_border_index) do
+  def flip(%Picture{} = picture) do
+    Picture.new(Enum.reverse(picture.lines))
+  end
+
+  def rotate(%Tile{} = tile) do
+    max = length(tile.lines) - 1
+
+    Tile.new(
+      tile.id,
+      Enum.map(max..0, fn index ->
+        column(tile, index)
+      end)
+    )
+  end
+
+  def rotate(%Picture{} = picture) do
+    max = length(picture.lines) - 1
+
+    Picture.new(
+      Enum.map(max..0, fn index ->
+        column(picture, index)
+      end)
+    )
+  end
+
+  def variations(variable) do
+    variable
+    |> Stream.iterate(&rotate/1)
+    |> Stream.take(4)
+    |> Stream.concat(Stream.iterate(flip(variable), &rotate/1) |> Stream.take(4))
+  end
+
+  def column(%{lines: lines}, index) do
+    column(lines, index)
+  end
+
+  def column(lines, index) do
+    max = length(lines) - 1
+
+    Enum.reduce(lines, 0, fn line, acc ->
+      acc <<< 1 ||| (line >>> (max - index) &&& 1)
+    end)
+  end
+
+  def find_neighbor(tiles, target, target_border_index) do
     target_border = Enum.at(target.borders, target_border_index)
 
     tiles
-    |> Stream.flat_map(fn {_id, tile} ->
-      if tile.id in used do
-        []
-      else
-        variations(tile)
-      end
-    end)
+    |> Stream.flat_map(fn {_id, tile} -> variations(tile) end)
     |> Enum.find(fn tile ->
       Enum.at(tile.borders, rem(target_border_index + 2, 4)) == target_border
     end)
     |> (fn
           nil -> nil
-          tile -> {tile.id, Map.put(tiles, tile.id, tile)}
+          tile -> {tile, Map.delete(tiles, tile.id)}
         end).()
   end
 
-  def big_picture(tiles, map, square_side) do
+  def big_picture(map, square_side) do
     0..(square_side - 1)
     |> Enum.flat_map(fn y ->
       0..(square_side - 1)
       |> Enum.map(fn x ->
-        tiles
-        |> Map.get(map[{x, y}])
+        map
+        |> Map.get({x, y})
         |> Map.get(:lines)
         |> Enum.drop(1)
         |> Enum.take(8)
@@ -190,12 +140,8 @@ defmodule Camera do
     |> Picture.new()
   end
 
-  def render_line(int, size) do
-    int |> Integer.to_string(2) |> String.pad_leading(size, "0")
-  end
-
   def hunt(picture) do
-    bit_size = String.length(hd(@monster))
+    monster_size = @monster |> hd() |> String.length()
     side = Picture.side(picture)
 
     monster_mask =
@@ -203,40 +149,44 @@ defmodule Camera do
         line
         |> String.graphemes()
         |> Enum.reduce(0, fn chr, acc ->
-          acc <<< 1 ||| (if chr == "#", do: 1, else: 0)
+          acc <<< 1 ||| if chr == "#", do: 1, else: 0
         end)
       end)
 
     picture
-    |> Picture.variations()
+    |> variations()
     |> Enum.find_value(fn picture ->
       picture.lines
       |> Stream.zip(Stream.drop(picture.lines, 1))
       |> Stream.zip(Stream.drop(picture.lines, 2))
       |> Stream.map(fn {{a, b}, c} -> [a, b, c] end)
       |> Enum.reduce(0, fn lines, count ->
-        0..(side - bit_size)
+        0..(side - monster_size)
         |> Enum.count(fn shift ->
           lines
-          |> Enum.map(&(&1 >>> shift))
-          |> Enum.zip(monster_mask)
+          |> Stream.map(&(&1 >>> shift))
+          |> Stream.zip(monster_mask)
           |> Enum.reduce(true, fn {int, mask}, acc ->
-            acc && ((int &&& mask) == mask)
+            acc && (int &&& mask) == mask
           end)
         end)
         |> Kernel.+(count)
       end)
       |> (fn
-        0 -> nil
-        other -> other
-      end).()
+            0 -> nil
+            other -> other
+          end).()
     end)
+  end
+
+  def roughness(picture) do
+    roughness(picture, hunt(picture))
   end
 
   def roughness(picture, monsters) do
     side = Picture.side(picture)
 
-    by_monsters =
+    by_monster =
       @monster
       |> Enum.flat_map(&String.graphemes/1)
       |> Enum.count(fn chr -> chr == "#" end)
@@ -245,79 +195,106 @@ defmodule Camera do
     |> Enum.reduce(0, fn line, acc ->
       0..side
       |> Enum.reduce(0, fn shift, ones ->
-        ones + ((line >>> shift) &&& 1)
+        ones + (line >>> shift &&& 1)
       end)
       |> Kernel.+(acc)
     end)
-    |> Kernel.-(monsters * by_monsters)
+    |> Kernel.-(monsters * by_monster)
+  end
+
+  def find_corners(tiles) do
+    flip = fn
+      _int, acc, 10, _then ->
+        acc
+
+      int, acc, count, then ->
+        then.(int, acc <<< 1 ||| (int >>> count &&& 1), count + 1, then)
+    end
+
+    flipped = fn borders ->
+      Enum.map(borders, &flip.(&1, 0, 0, flip))
+    end
+
+    corner_borders =
+      tiles
+      |> Enum.flat_map(fn {_id, tile} ->
+        tile.borders ++ flipped.(tile.borders)
+      end)
+      |> Enum.frequencies()
+      |> Enum.filter(&match?({_border, 1}, &1))
+      |> Enum.map(&elem(&1, 0))
+      |> MapSet.new()
+
+    tiles
+    |> Enum.filter(fn {_id, tile} ->
+      4 ==
+        tile.borders
+        |> MapSet.new()
+        |> MapSet.union(MapSet.new(flipped.(tile.borders)))
+        |> MapSet.intersection(corner_borders)
+        |> MapSet.size()
+    end)
+    |> Enum.into(%{})
+  end
+
+  def build_picture(input) do
+    {map, square_side} = build_map(input)
+    big_picture(map, square_side)
+  end
+
+  def build_map(input) do
+    tiles = input |> Camera.parse() |> Enum.into(%{}, fn tile -> {tile.id, tile} end)
+    square_side = tiles |> map_size() |> :math.sqrt() |> floor()
+    corner_tiles = find_corners(tiles)
+
+    # "top left" corner
+    corner =
+      corner_tiles
+      |> Enum.find(fn {_id, corner} ->
+        find_neighbor(Map.delete(tiles, corner.id), corner, 0) == nil &&
+          find_neighbor(Map.delete(tiles, corner.id), corner, 3) == nil
+      end)
+      |> elem(1)
+
+    build_column = fn map, tiles, x, head ->
+      map = Map.put(map, {x, 0}, head)
+
+      1..(square_side - 1)
+      |> Enum.reduce({map, tiles}, fn y, {map, tiles} ->
+        position = {x, y}
+        {tile, tiles} = find_neighbor(tiles, map[{x, y - 1}], 2)
+        map = Map.put(map, position, tile)
+
+        {map, tiles}
+      end)
+    end
+
+    tiles = Map.delete(tiles, corner.id)
+    map = %{{0, 0} => corner}
+
+    {map, tiles} =
+      Enum.reduce(0..(square_side - 1), {map, tiles}, fn x, {map, tiles} ->
+        map =
+          Map.put_new_lazy(map, {x, 0}, fn ->
+            tiles
+            |> Camera.find_neighbor(map[{x - 1, 0}], 1)
+            |> elem(0)
+          end)
+
+        tiles = Map.delete(tiles, map[{x, 0}].id)
+
+        build_column.(map, tiles, x, map[{x, 0}])
+      end)
+
+    if map_size(tiles) > 0 do
+      raise "There are tiles left"
+    end
+
+    {map, square_side}
   end
 end
 
-tiles = "input.txt" |> Camera.parse() |> Enum.into(%{}, fn tile -> {tile.id, tile} end)
-
-square_side = tiles |> map_size() |> :math.sqrt() |> floor()
-
-corner_borders =
-  tiles
-  |> Enum.flat_map(fn {_id, tile} -> tile.borders ++ tile.borders_flipped end)
-  |> Enum.frequencies()
-  |> Enum.filter(&match?({_border, 1}, &1))
-  |> Enum.map(&elem(&1, 0))
-  |> MapSet.new()
-
-corner_tiles =
-  tiles
-  |> Enum.filter(fn {_id, tile} ->
-    4 ==
-      tile.borders
-      |> MapSet.new()
-      |> MapSet.union(MapSet.new(tile.borders_flipped))
-      |> MapSet.intersection(corner_borders)
-      |> MapSet.size()
-  end)
-  |> Enum.into(%{})
-
-corner =
-  corner_tiles
-  |> Enum.find(fn {_id, corner} ->
-    Camera.find_candidate(tiles, MapSet.new([corner.id]), corner, 0) == nil &&
-      Camera.find_candidate(tiles, MapSet.new([corner.id]), corner, 3) == nil
-  end)
-  |> elem(1)
-
-tiles = Map.put(tiles, corner.id, corner)
-used = MapSet.new([corner.id])
-map = %{{0, 0} => corner.id}
-
-{tiles, used, map} =
-  1..(square_side - 1)
-  |> Enum.reduce({tiles, used, map}, fn n, {tiles, used, map} ->
-    position = {0, n}
-    {id, tiles} = Camera.find_candidate(tiles, used, tiles[map[{0, n - 1}]], 2)
-    map = Map.put(map, position, id)
-
-    {tiles, MapSet.put(used, id), map}
-  end)
-
-{tiles, _used, map} =
-  Enum.reduce(1..(square_side - 1), {tiles, used, map}, fn x, {tiles, used, map} ->
-    {id, tiles} = Camera.find_candidate(tiles, used, tiles[map[{x - 1, 0}]], 1)
-    map = Map.put(map, {x, 0}, id)
-    used = MapSet.put(used, id)
-
-    1..(square_side - 1)
-    |> Enum.reduce({tiles, used, map}, fn y, {tiles, used, map} ->
-      position = {x, y}
-      {id, tiles} = Camera.find_candidate(tiles, used, tiles[map[{x, y - 1}]], 2)
-      map = Map.put(map, position, id)
-
-      {tiles, MapSet.put(used, id), map}
-    end)
-  end)
-
-picture = Camera.big_picture(tiles, map, square_side)
-monsters = Camera.hunt(picture)
-
-picture
-|> Camera.roughness(monsters)
+"input.txt"
+|> Camera.build_picture()
+|> Camera.roughness()
 |> IO.inspect()
